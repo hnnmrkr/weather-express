d74b4c0125b58d6721ad3860e4df77a52fe10352
Hanna <hanna-marii.kriisa@voco.ee>
2023-01-04 11:27:36 +0200

    created project basic files

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+const {dirname} = require("path");
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', function(req, res){
+    res.send('Hello World')
+})
+
+app.listen(3000)
